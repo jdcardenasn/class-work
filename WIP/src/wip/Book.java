d7d2f4cefd018cf7c8bb5de5139da7f6f0f55f86
jdcardenasn <jdcardenasn@unal.edu.co>
2016-09-27 01:02:41 -0500

    actulizando estado

---
+++ WIP/src/wip/Book.java
@@ -0,0 +1,12 @@
+package wip;
+/**
+ *
+ * @author Juan
+ */
+class Book {
+    private String author;
+    private String title;
+    public Book(String Author, String name,double weight){
+        
+    }    
+}
