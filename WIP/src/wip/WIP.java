a18bd3d99957fc40d9e998cc8fd9a66adebbb565
jdcardenasn <jdcardenasn@unal.edu.co>
2016-09-26 23:32:35 -0500

    actuliando estado

---
+++ WIP/src/wip/WIP.java
@@ -0,0 +1,16 @@
+package wip;
+
+/**
+ *
+ * @author Juan
+ */
+public class WIP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
